2da64f707c7c72f14b2838be1c70ceb9e544086e
Natalia Stan <popkids.ua@gmail.com>
2025-06-12 18:24:16 +0300

    add style for SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,61 +1,125 @@
 import { Field, Form, Formik } from "formik";
 import css from "./SearchBar.module.css";
-import Select from "react-select";
-import { customSelectStyles } from "./СommonSelectStyle.js";
+import Select, { components } from "react-select";
+import { CustomSelectStyles } from "./СustomSelectStyle.js";
+
+const CustomDropdownIndicator = (props) => {
+  const { menuIsOpen } = props.selectProps;
+
+  return (
+    <components.DropdownIndicator {...props}>
+      <svg
+        width="16"
+        height="16"
+        style={{
+          transform: menuIsOpen ? "rotate(180deg)" : "rotate(0deg)",
+          transition: "transform 0.4s ease",
+        }}
+      >
+        <use xlinkHref="/icons.svg#icon-down" />
+      </svg>
+    </components.DropdownIndicator>
+  );
+};
+
+const CustomSingleValue = ({ data, ...props }) => (
+  <components.SingleValue {...props}>To ${data.label}</components.SingleValue>
+);
+
+const customComponents = {
+  DropdownIndicator: CustomDropdownIndicator,
+  IndicatorSeparator: () => null,
+  SingleValue: CustomSingleValue,
+};
+
+const stylesBrandSelector = {
+  ...CustomSelectStyles,
+  menuList: (base) => ({ ...base, maxHeight: 272 }),
+};
+
+const stylesPriceSelector = {
+  ...CustomSelectStyles,
+  menuList: (base) => ({ ...base, maxHeight: 188 }),
+};
 
 const SearchBar = () => {
   return (
     <Formik
       initialValues={{
         brand: "",
-        price: "",
-        minMileage: "",
-        maxMileage: "",
+        price: null,
+        from: "",
+        to: "",
       }}
       onSubmit={(values) => {
         console.log(values);
       }}
     >
-      <Form className={css.form}>
-        <div className={css.wrapper}>
-          <label className={css.label} name="brand">
-            Car brand
-          </label>
-          <Select
-            name="brand"
-            type="select"
-            placeholder="Choose a brand"
-            styles={customSelectStyles}
-          />
-        </div>
+      {({ values, setFieldValue }) => (
+        <Form className={css.form}>
+          <div className={css.wrapper}>
+            <label className={css.label} htmlFor="brand">
+              Car brand
+            </label>
+            <Select
+              name="brand"
+              placeholder="Choose a brand"
+              styles={stylesBrandSelector}
+              components={customComponents}
+            />
+          </div>
 
-        <div className={css.wrapper}>
-          <label className={css.label} name="price">
-            Price/ 1 hour
-          </label>
-          <Select
-            name="price"
-            type="select"
-            placeholder="Choose a price"
-            styles={customSelectStyles}
-          />
-        </div>
+          <div className={css.wrapper}>
+            <label className={css.label} htmlFor="price">
+              Price/ 1 hour
+            </label>
+            <Select
+              name="price"
+              placeholder="Choose a price"
+              styles={stylesPriceSelector}
+              components={customComponents}
+              options={[
+                { value: 30, label: "30" },
+                { value: 40, label: "40" },
+                { value: 50, label: "50" },
+                { value: 60, label: "60" },
+                { value: 70, label: "70" },
+                { value: 80, label: "80" },
+                { value: 90, label: "90" },
+                { value: 100, label: "100" },
+                { value: 110, label: "110" },
+                { value: 120, label: "120" },
+              ]}
+              value={values.price}
+              onChange={(selectedOption) =>
+                setFieldValue("price", selectedOption)
+              }
+              formatValueLabel={(e) => `To $${e.label}`}
+              isSearchable={false}
+            />
+          </div>
 
-        <div className={css.wrapper}>
-          <label className={css.label} name="mileage">
-            Сar mileage / km
-          </label>
-          <Field
-            name="mileage"
-            type="text"
-            placeholder="Comment"
-            className={css.input}
-          />
-        </div>
-        <button className={css.btn} type="submit">
-          Search
-        </button>
-      </Form>
+          <div className={css.wrapper}>
+            <label className={css.label} name="from">
+              Сar mileage / km
+            </label>
+            <div className={css.rangeInputGroup}>
+              <div className={css.inputWrapper}>
+                <span className={css.inputLabel}>From</span>
+                <input type="number" name="from" className={css.rangeInput} />
+              </div>
+              <span className={css.separator}></span>
+              <div className={css.inputWrapper}>
+                <span className={css.inputLabel}>To</span>
+                <input type="number" name="to" className={css.rangeInput} />
+              </div>
+            </div>
+          </div>
+          <button className={css.btn} type="submit">
+            Search
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
---
+++ src/components/SearchBar/СustomSelectStyle.js
@@ -0,0 +1,43 @@
+export const CustomSelectStyles = {
+  singleValue: (base) => ({
+    ...base,
+    justifyContent: "flex-start",
+    display: "flex",
+    width: "100%",
+  }),
+  control: (base, state) => ({
+    ...base,
+    backgroundColor: "#f7f7f7",
+    border: "none",
+    borderRadius: "12px",
+    padding: "4px 4px",
+    outline: "none",
+    boxShadow: "none",
+    minHeight: "44px",
+    minWidth: "202px",
+  }),
+  option: (base, state) => ({
+    ...base,
+    height: "30px",
+    color: state.isSelected ? "#101828" : "#8d929a",
+    backgroundColor:
+      state.isSelected || state.isFocused ? "transparent" : "transparent",
+    padding: "14px 18px",
+    lineHeight: "1.25",
+    textAlign: "left",
+    fontFamily: "'Manrope', sans-serif",
+    ":active": {
+      backgroundColor: "transparent",
+    },
+  }),
+  placeholder: (base) => ({
+    ...base,
+    color: "#101828",
+    marginRight: "32px",
+  }),
+  menu: (base) => ({
+    ...base,
+    borderRadius: "10px",
+    zIndex: 20,
+  }),
+};
